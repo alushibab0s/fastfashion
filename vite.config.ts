2b1710a24866de75d9a53e60cee91c369d4f7454
alushibab0s <balonmarlon069@gmail.com>
2025-05-17 17:45:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/first-time-react/',
+  base: '/fastfashion/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
